e92df363d863ddb4c633276768ce5a6510b9d53f
Lucas Silva Dantas Abrantes <lucasabrantes002@gmail.com>
2021-07-21 22:48:54 -0300

    Lidando com seeders

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,13 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
-  public async index({ request, auth }: HttpContextContract) {
+  public async store({ request, auth }: HttpContextContract) {
     const { email, password } = request.all()
 
     const token = await auth.attempt(email, password, {
       expiresIn: '30',
     })
-
     return token
   }
 
