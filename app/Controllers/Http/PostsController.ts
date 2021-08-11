cc05338a91b275defea6c6eb3369cde3be676e6c
Lucas Silva Dantas Abrantes <lucasabrantes002@gmail.com>
2021-08-10 22:06:23 -0300

    Serialize data

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -4,7 +4,7 @@ import { StoreValidator, UpdateValidator } from 'App/Validators/Post'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {
-    const posts = await Post.all()
+    const posts = await Post.query().orderBy('id', 'asc').preload('author')
     return posts
   }
 
